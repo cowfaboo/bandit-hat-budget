a4dc06fb534368a622dbc58ac33b026908636683
Daniel Gauthier <daniel.m.gauthier@icloud.com>
2016-11-24 00:36:15 -0500

    Initial Commit

---
+++ Budget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Budget
+//
+//  Created by Daniel Gauthier on 2016-11-24.
+//  Copyright © 2016 Bandit Hat Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
